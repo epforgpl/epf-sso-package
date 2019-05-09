865db3f7847f803435e0075f5978ab2e883c56d0
Jan Zankowski <jan.zankowski+signup@gmail.com>
2019-05-09 13:26:28 +0200

    Move non business logic code to top level directory, out of "src".

--- src/EpfSsoServiceProvider.php
+++ src/EpfSsoServiceProvider.php
@@ -8,9 +8,9 @@ class EpfSsoServiceProvider extends ServiceProvider {
 
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
-        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
-        $this->loadViewsFrom(__DIR__ . '/resources/views', 'epf-sso');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'epf-sso');
         $this->publishes([
             __DIR__ . '/../public' => public_path('vendor/epforgpl/epf-sso-package'),
         ], 'public');

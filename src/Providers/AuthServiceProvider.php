02bba3ae5a2f4e6b66bdb148096dd39b009c5742
Jan Zankowski <jan.zankowski+signup@gmail.com>
2019-05-13 17:17:16 +0200

    Changing passwords. | Fix 1.

--- src/Providers/AuthServiceProvider.php
+++ src/Providers/AuthServiceProvider.php
@@ -2,9 +2,12 @@
 
 namespace EpfOrgPl\EpfSso\Providers;
 
+use EpfOrgPl\EpfSso\Auth\EpfHasher;
 use EpfOrgPl\EpfSso\Auth\EpfUserProvider;
-use Illuminate\Support\Facades\Auth;
+use EpfOrgPl\EpfSso\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -22,9 +25,21 @@ class AuthServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(EpfHasher $hasher)
     {
         $this->registerPolicies();
+
+        Validator::extend('matches_current_password', function ($attr, $value, $params, $validator) use ($hasher) {
+            if (!Auth::check()) {
+                throw new \Exception('"matches_current_password" validation rule should only be used on forms '
+                    . 'available to logged-in users.');
+            }
+            return $hasher->check($value, Auth::user()->password, ['hash_type' => Auth::user()->hash_type]);
+        });
+
+        Validator::extend('is_registered_user', function ($attr, $value, $params, $validator) {
+            return (User::whereEmail($value)->first() !== null);
+        });
 
         Auth::provider('epf', function ($app, array $config) {
             return new EpfUserProvider($this->app['epfhash'], $this->app['config']['auth.providers.users.model']);

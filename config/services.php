1dfbdbe753d0006d886026b44dd8c971f7b3fd47
Jan Zankowski <jan.zankowski+signup@gmail.com>
2019-05-15 16:41:44 +0200

    Social login. | Fix 2.

--- config/services.php
+++ config/services.php
@@ -2,17 +2,7 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Third Party Services
-    |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Stripe, Mailgun, SparkPost and others. This file provides a sane
-    | default location for this type of information, allowing packages
-    | to have a conventional place to find your various credentials.
-    |
-    */
+    // Social login providers credentials.
 
     'facebook' => [
         'client_id' => env('SIGN_IN_W_FACEBOOK_CLIENT_ID'),
